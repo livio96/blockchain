e5288788b185b311286451f85d7429d2eb5e6c8a
= <=>
2018-01-09 16:11:03 -0500

    create a blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,71 +1,64 @@
 
-import hashlib
-import json
-from time import time 
-from uuid import uuid4
+#Create a blockchain from scratch 
+# This is called SNAKECOIN (*_-)
 
 
-class Blockchain(object):
-	def __init__(self):
-		self.chain = []
-		self.current_transactions = [] 
-		#create a new genesis block (aka first block)
-		self.new_block(previous_hash=1, proof = 100)
+import hashlib as hasher 
+import datetime as date  
+
+class Block:
+	def __init__(self,index,timestamp,data,previous_hash):
+		self.index = index 
+		self.timestamp = timestamp
+		self.data = data
+		self.previous_hash = previous_hash
+		self.hash = self.hash_block() 
+
+	def hash_block(self):
+		sha = hasher.sha256()
+		sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode('utf-8'))
+		return sha.hexdigest()
+
+def create_genesis_block():
+		#manually construct a block with index zero
+		return Block(0, date.datetime.now(), "Genesis Block" , "0")
+
+
+def next_block(last_block):
+		this_index = last_block.index +1 
+		this_timestamp = date.datetime.now() 
+		this_data = "Hey! I'm block" + str(this_index)
+		this_hash = last_block.hash
+		return Block(this_index, this_timestamp, this_data, this_hash)
+
+
+#create the blockchain by connection all blocks together
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+#how many blocks should we add to the chain
+#after the genesis block(which is the first one)
 	
-	def new_block(self):
-		#create a new block 
+num_of_blocks_to_add = 4
 
-		block = {
-		'index':len(self.chain) + 1 ,
-		'timestamp':time(),
-		'transactions': self.current_transactions,
-		'proof': proof, 
-		'previous_hash':previous_hash or self.hash(self.chain[-1])		   }
+#add blocks using a for loop 
+for i in range(0,num_of_blocks_to_add):
+	block_to_add = next_block(previous_block)
+	blockchain.append(block_to_add)
+	previous_block = block_to_add
 
-		#reset list
-		self.current_transactions = [] 
-		
-		self.chain.append(block)
-		return block 
-
-		
-		
-
-	def new_transaction(self, sender, recipient, amount):
-		#create a new transaction inside a block 
-		self.current_transactions.append ( {
-		'sender': sender , 
-		'recipient':recipient,
-		'amount': amount,
-		})
-		
-		return self.last_block['index'] + 1
-		
-
-	@staticmethod
-	def hash(block):
-		#haches a block 
-		block_string = json.dumps(block, sort_keys=True).encode()
-		return hashlib.sha256(block_string).hexdigest()
-
-
-
-	@property
-	def last_block(self):
-		#keeps the track of the last block in the chain
-		return self.chain[-1]
-
-
+	#Print out the whole blockchain
+	print()
+	print("Block " + str(block_to_add.index) + " has been added successfully!!!" )
+	print("Hash : {}\n".format(block_to_add.hash))
+	print("Block data : " , block_to_add.data)
+	if(block_to_add == create_genesis_block() ):
+		print("No previous hash")
+	else:
+		print("Previous hash = ", block_to_add.previous_hash) 
 	
-	def proof_of_work(self, last_proof):
-		proof = 0 
-		while self.valid_proof(last_proof, proof) is False:
-			proof+=1
-		return proof
+	
 
-	def valid_proof(last_proof, proof):
-		guess = f'{last_proof}{proof}'.encode()
-		guess.hash = hashlib.sha256(guess).hexdigest() 
-		return guess.hash[:4] =="0000"
 
-		
+
+
